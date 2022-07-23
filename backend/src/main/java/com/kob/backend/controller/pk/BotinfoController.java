41c3bf16c0c4640b22da3fa80b000e5ef1252962
May observer <984178984@qq.com>
2022-07-23 10:31:08 +0800

    项目骨架

--- backend/src/main/java/com/kob/backend/controller/pk/BotinfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotinfoController.java
@@ -3,13 +3,19 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 //默认返回一个数据，controller是返回一个模板
 @RequestMapping("/pk/")
 public class BotinfoController {
 
     @RequestMapping("getbotinfo/")
-    public String getBotInfo(){
-        return "hhh";
+    public Map<String, String> getBotInfo(){
+        Map<String, String> bot1 =new HashMap<>();
+        bot1.put("name", "tiger");
+        bot1.put("rating", "1500");
+        return bot1;
     }
 }
--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,43 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>
+    <div>bot昵称：{{bot_name}}</div>
+    <div>bot战力：{{bot_rating}}</div>
+  </div>
   <router-view/>
 </template>
+
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+
+
+export default{
+  name: "App",
+  setup: ()=>{//函数入口
+    let bot_name = ref("");
+    let bot_rating = ref("");
+
+    $.ajax({
+      url: "http://127.0.0.1:3000/pk/getbotinfo/",
+      type:"get",
+      success(resp){
+        console.log(resp);
+        bot_name.value = resp.name;//注意是value ，？？？为什么
+        
+        bot_rating.value = resp.rating;
+      }
+    });
+
+    return {
+      bot_name,
+      bot_rating,
+    }
+  }
+  
+}
+
+</script>
+
 
 <style>
 #app {
